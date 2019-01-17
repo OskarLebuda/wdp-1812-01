19a9ef70e5ef1d587b9e12a524cce58a06d8012a
Łukasz Kulbacki <lukasz.kulbacki@gmail.com>
2019-01-17 21:35:16 +0100

    Save vote when clicked on the star (setVotedStars function)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,34 @@
-console.log('hello');
+(function () {
+
+    var starContainers = document.getElementsByClassName('stars');
+
+    window.dev = {};
+    window.dev.starContainer = starContainer;
+
+    var setVotedStars = function (e) {
+        e.target.parentElement.classList.add('voted');
+        if (e.target.tagName === 'A') {
+            var origStar = e.target;
+            var star = origStar;
+            do {
+                star.classList.add('full');
+                star = star.previousElementSibling;
+            }
+            while (star);
+            var star = origStar.nextElementSibling;
+            if (star) {
+                do {
+                    star.classList.remove('full');
+                    star = star.nextElementSibling;
+                }
+                while (star);
+            }
+        }
+    };
+
+    for (var i = 0; i < starContainers.length; i++) {
+        starContainers[i].addEventListener('click', setVotedStars);
+    }
+
+
+})();
